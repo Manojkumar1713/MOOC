9c9020774f756a3cb69299b1f232a0b754e78c4d
ManojKumar1713 <manojkumar.mamidi0193@gmail.com>
2021-04-07 13:42:37 +0530

    Adding index Page

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>KARE MOOC Platform</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="./Staff/images/kare_icon.ico" />
+	<link rel="stylesheet" type="text/css" href="./Staff/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/css/util.css">
+	<link rel="stylesheet" type="text/css" href="./Staff/css/main.css">
+
+<style>
+
+.footer {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  color: white;
+  text-align: center;
+}
+
+</style>
+
+</head>
+<body>
+
+
+<div class="limiter">
+<div class="container-login100" style="background-image: url('./Staff/images/kare_background.jpg');">
+<div class="wrap-login100 ">
+
+<div class="login100-form-avatar">
+	<a href="./index.php"><img src="./Staff/images/kare_logo.png" alt="AVATAR"></a>
+</div>
+	<span class="login100-form-title p-t-20 p-b-45">
+		Kalasalingam Academy of Research and Education <br>
+		<font style="color: black;"> KARE MOOC Platform </font>
+	</span>
+	<a href="./Staff/index.php">
+	<span class="login100-form-title p-t-20 p-b-45">
+		STAFF LOGIN
+	</span>
+	</a> 
+	<a href="./Student/index.php">
+	<span class="login100-form-title p-t-20 p-b-45">
+		STUDENT LOGIN
+	</span>
+	</a>
+	<a href="./Admin/index.php">
+	<span class="login100-form-title p-t-20 p-b-45">
+		ADMIN LOGIN
+	</span>
+	</a>
+</div>
+
+
+<div class="footer"><a href="./Developers/Developers.php" style="color: white;">Made with 💚 for</a> <a href="./Developers/Developers.php" style="color: white; background-color: black;">KARE</a>
+</div>
+
+</div>
+</div>
+</div>
+
+<script src="./Staff/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="./Staff/vendor/bootstrap/js/popper.js"></script>
+<script src="./Staff/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="./Staff/vendor/select2/select2.min.js"></script>
+<script src="./Staff/js/main.js"></script>
+
+
+
+</body>
+</html>
